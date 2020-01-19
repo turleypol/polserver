bcc0e5af8799f1d586bc869655a608ef27475597
turleypol <turley@polserver.com>
2020-01-19 11:23:11 +0100

    new bench

--- cmake/Benchmark.cmake
+++ cmake/Benchmark.cmake
@@ -3,6 +3,6 @@ if (ENABLE_BENCHMARK)
   message("* benchmark")
   set(BENCHMARK_ENABLE_TESTING OFF)
 
-  add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../lib/google-benchmark/" "${CMAKE_CURRENT_BINARY_DIR}/lib/google-benchmark" EXCLUDE_FROM_ALL)
-  set_compile_flags(benchmark 0)
+  add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../lib/benchmark-1.5.0/" "${CMAKE_CURRENT_BINARY_DIR}/lib/benchmark-1.5.0" EXCLUDE_FROM_ALL)
+#  set_compile_flags(benchmark 0)
 endif()
