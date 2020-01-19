250d0ebf54cfd5a8c7bed81dd9b15654d95feabc
turleypol <turley@polserver.com>
2020-01-19 11:47:36 +0100

    i

--- cmake/Benchmark.cmake
+++ cmake/Benchmark.cmake
@@ -2,6 +2,7 @@
 if (ENABLE_BENCHMARK)
   message("* benchmark")
   set(BENCHMARK_ENABLE_TESTING OFF)
+  set(BENCHMARK_ENABLE_GTEST_TESTS OFF)
 
   add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/../lib/benchmark-1.5.0/" "${CMAKE_CURRENT_BINARY_DIR}/lib/benchmark-1.5.0" EXCLUDE_FROM_ALL)
 #  set_compile_flags(benchmark 0)
