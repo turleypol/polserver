dee6ee79b76e92d2ab0f3e1993efdddad2e7e0c1
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:20 -0700

    Remainder SourceFileProcessor

--- pol-core/bscript/compiler/astbuilder/SourceFileProcessor.h
+++ pol-core/bscript/compiler/astbuilder/SourceFileProcessor.h
@@ -7,7 +7,7 @@
 #include <memory>
 #include <string>
 
-#include "DeclarationBuilder.h"
+#include "ProgramBuilder.h"
 
 namespace Pol::Bscript::Compiler
 {
@@ -22,10 +22,30 @@ public:
 public:
   long long use_module( const std::string& name, SourceLocation& including_location );
   void process_source( SourceFile& );
+  void process_include( long long* us_counted, SourceFile& );
+
+  void handle_include_declaration( EscriptGrammar::EscriptParser::IncludeDeclarationContext*,
+                                   long long* us_spent );
+  boost::optional<std::string> locate_include_file( const SourceLocation& source_location,
+                                                    const std::string& include_name );
+
+  long long handle_use_declaration( EscriptGrammar::EscriptParser::UseDeclarationContext* );
 
   antlrcpp::Any visitStatement( EscriptGrammar::EscriptParser::StatementContext* ) override;
   antlrcpp::Any visitUnambiguousStatement(
       EscriptGrammar::EscriptParser::UnambiguousStatementContext* ) override;
+  antlrcpp::Any visitProgramDeclaration(
+      EscriptGrammar::EscriptParser::ProgramDeclarationContext* ) override;
+  antlrcpp::Any visitUnambiguousProgramDeclaration(
+      EscriptGrammar::EscriptParser::UnambiguousProgramDeclarationContext* ) override;
+  antlrcpp::Any visitIncludeDeclaration(
+      EscriptGrammar::EscriptParser::IncludeDeclarationContext* ) override;
+  antlrcpp::Any visitUseDeclaration(
+      EscriptGrammar::EscriptParser::UseDeclarationContext* ) override;
+  antlrcpp::Any visitFunctionDeclaration(
+      EscriptGrammar::EscriptParser::FunctionDeclarationContext* ) override;
+  antlrcpp::Any visitUnambiguousFunctionDeclaration(
+      EscriptGrammar::EscriptParser::UnambiguousFunctionDeclarationContext* ) override;
 
   SourceLocation location_for( antlr4::ParserRuleContext& ) const;
 
@@ -35,7 +55,7 @@ protected:
   const SourceFileIdentifier& source_file_identifier;
   BuilderWorkspace& workspace;
 
-  DeclarationBuilder tree_builder;
+  ProgramBuilder tree_builder;
 
 private:
   const bool is_src;
