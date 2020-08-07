d470899d0579c414ba38fce2cae2fcc646878546
Eric Swanson <eric.the.unicorn@gmail.com>
2020-08-06 21:15:11 -0700

    ValueBuilder for IntegerValue

--- pol-core/bscript/compiler/astbuilder/ValueBuilder.h
+++ pol-core/bscript/compiler/astbuilder/ValueBuilder.h
@@ -7,6 +7,7 @@ namespace Pol::Bscript::Compiler
 {
 class FloatValue;
 class FunctionReference;
+class IntegerValue;
 class StringValue;
 class Value;
 
@@ -18,9 +19,11 @@ public:
   std::unique_ptr<FloatValue> float_value(
       EscriptGrammar::EscriptParser::FloatLiteralContext* );
 
-
   std::unique_ptr<FunctionReference> function_reference(
       EscriptGrammar::EscriptParser::FunctionReferenceContext* );
+
+  std::unique_ptr<IntegerValue> integer_value(
+      EscriptGrammar::EscriptParser::IntegerLiteralContext* );
 
   std::unique_ptr<StringValue> string_value( antlr4::tree::TerminalNode* string_literal );
 
@@ -29,6 +32,7 @@ public:
   std::unique_ptr<Value> value( EscriptGrammar::EscriptParser::LiteralContext* );
 
 private:
+  int to_int( EscriptGrammar::EscriptParser::IntegerLiteralContext* );
 };
 
 }  // namespace Pol::Bscript::Compiler
