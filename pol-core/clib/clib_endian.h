27407774ea9a7dc76220111c48605d7a8dbb7d1b
turleypol <turley@polserver.com>
2023-07-09 18:19:28 +0200

    test3

--- pol-core/clib/clib_endian.h
+++ pol-core/clib/clib_endian.h
@@ -3,6 +3,9 @@
 #include "rawtypes.h"
 #include <type_traits>
 
+namespace Pol
+{
+
 namespace
 {
 
@@ -18,66 +21,71 @@ template <typename T>
   static_assert( std::is_integral_v<T> && sizeof( T ) <= 8,
                  "flipEndian supports only integral types with size up to 8 bytes" );
 
-  if constexpr ( sizeof( T ) == 2 )
+  if constexpr ( std::is_signed_v<T> )
   {
-    return static_cast<T>( ( value >> 8 ) | ( value << 8 ) );
+    // Handle signed integral types separately
+    using UnsignedT = std::make_unsigned_t<T>;
+    return static_cast<T>( flipEndian( static_cast<UnsignedT>( value ) ) );
   }
-  else if constexpr ( sizeof( T ) == 4 )
+  else
   {
-    return static_cast<T>( ( value >> 24 ) | ( ( value >> 8 ) & 0xFF00 ) |
-                           ( ( value << 8 ) & 0xFF0000 ) | ( value << 24 ) );
+    if constexpr ( sizeof( T ) == 2 )
+    {
+      return static_cast<T>( ( value >> 8 ) | ( value << 8 ) );
+    }
+    else if constexpr ( sizeof( T ) == 4 )
+    {
+      return static_cast<T>( ( value >> 24 ) | ( ( value >> 8 ) & 0xFF00 ) |
+                             ( ( value << 8 ) & 0xFF0000 ) | ( value << 24 ) );
+    }
+    else if constexpr ( sizeof( T ) == 8 )
+    {
+      return static_cast<T>( ( value >> 56 ) | ( ( value >> 40 ) & 0xFF00 ) |
+                             ( ( value >> 24 ) & 0xFF0000 ) | ( ( value >> 8 ) & 0xFF000000 ) |
+                             ( ( value << 8 ) & 0xFF00000000 ) |
+                             ( ( value << 24 ) & 0xFF0000000000 ) |
+                             ( ( value << 40 ) & 0xFF000000000000 ) | ( value << 56 ) );
+    }
+    return value;
   }
-  else if constexpr ( sizeof( T ) == 8 )
-  {
-    return static_cast<T>( ( value >> 56 ) | ( ( value >> 40 ) & 0xFF00 ) |
-                           ( ( value >> 24 ) & 0xFF0000 ) | ( ( value >> 8 ) & 0xFF000000 ) |
-                           ( ( value << 8 ) & 0xFF00000000 ) |
-                           ( ( value << 24 ) & 0xFF0000000000 ) |
-                           ( ( value << 40 ) & 0xFF000000000000 ) | ( value << 56 ) );
-  }
-  return value;
 }
 
 }  // anonymous namespace
 
 template <typename T>
-[[nodiscard]] constexpr std::enable_if_t<sizeof( T ) == 2 && std::is_unsigned_v<T>, T> cfLEu16(
-    T value )
+[[nodiscard]] constexpr T cfLEu16( T value )
 {
-  return UseBigEndian ? value : flipEndian<T>( value );
+  return UseBigEndian ? value : flipEndian( value );
 }
 
 template <typename T>
-[[nodiscard]] constexpr std::enable_if_t<sizeof( T ) == 4 && std::is_unsigned_v<T>, T> cfLEu32(
-    T value )
+[[nodiscard]] constexpr T cfLEu32( T value )
 {
-  return UseBigEndian ? value : flipEndian<T>( value );
+  return UseBigEndian ? value : flipEndian( value );
 }
 
 template <typename T>
-[[nodiscard]] constexpr std::enable_if_t<sizeof( T ) == 2 && std::is_unsigned_v<T>, T> cfBEu16(
-    T value )
+[[nodiscard]] constexpr T cfBEu16( T value )
 {
-  return UseBigEndian ? flipEndian<T>( value ) : value;
+  return UseBigEndian ? flipEndian( value ) : value;
 }
 
 template <typename T>
-[[nodiscard]] constexpr std::enable_if_t<sizeof( T ) == 4 && std::is_unsigned_v<T>, T> cfBEu32(
-    T value )
+[[nodiscard]] constexpr T cfBEu32( T value )
 {
-  return UseBigEndian ? flipEndian<T>( value ) : value;
+  return UseBigEndian ? flipEndian( value ) : value;
 }
 
 template <typename T>
 [[nodiscard]] constexpr T cfLE( T value )
 {
-  return UseBigEndian ? flipEndian<T>( value ) : value;
+  return UseBigEndian ? flipEndian( value ) : value;
 }
 
 template <typename T>
 [[nodiscard]] constexpr T cfBE( T value )
 {
-  return UseBigEndian ? value : flipEndian<T>( value );
+  return UseBigEndian ? value : flipEndian( value );
 }
 
 // Aliases using statement
@@ -92,4 +100,6 @@ using ctBEu32 = decltype( cfBEu32<T> );
 
 template <typename T>
 using ctBEu16 = decltype( cfBEu16<T> );
+
+}  // namespace Pol
 
