c55042bca01fa3f52a4f8c89b9e30c4542f6c5a7
turleypol <turley@polserver.com>
2023-09-03 20:55:37 +0200

    include4

--- pol-core/pol/module/basiciomod.cpp
+++ pol-core/pol/module/basiciomod.cpp
@@ -23,11 +23,12 @@ Bscript::BObjectImp* BasicIoExecutorModule::mf_Print()
 {
   int color{ 0 };
   if ( !exec.getParam( 1, color ) )
-    return new BError( "Invalid parameter type" );
+    return new Bscript::BError( "Invalid parameter type" );
   if ( color > 0 && color < 100 )
   {
     constexpr char RESET_COLOR[] = "\x1b[0m";
-    INFO_PRINT << "\x1b[" << color << exec.getParamImp( 0 )->getStringRep() << RESET_COLOR << "\n";
+    INFO_PRINT << "\x1b[" << color << 'm' << exec.getParamImp( 0 )->getStringRep() << RESET_COLOR
+               << "\n";
   }
   else
   {
