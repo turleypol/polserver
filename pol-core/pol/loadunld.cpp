ae5894232b4546e8e80a7de788dd4ec0c1396642
turleypol <turley@polserver.com>
2023-10-03 13:57:45 +0200

    test2

--- pol-core/pol/loadunld.cpp
+++ pol-core/pol/loadunld.cpp
@@ -270,7 +270,7 @@ void reload_configuration()
   PolConfig::read_pol_config( false );
   ServSpecOpt::read_servspecopt();
   Network::read_bannedips_config( false );
-  unload_npc_templates();
+  gamestate.unload_npc_templates();
   load_npc_templates();
   read_npc_templates();  // dave 1/12/3 npc template data wasn't actually being read, just names.
   ConsoleCommand::load_console_commands();
