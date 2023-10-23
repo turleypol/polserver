947a68909a5520a441abaa8aa28a018acba3526b
turleypol <turley@polserver.com>
2023-10-23 11:25:10 +0200

    now

--- pol-core/clib/clib_MD5.cpp
+++ pol-core/clib/clib_MD5.cpp
@@ -96,10 +96,9 @@ namespace Clib
 {
 bool MD5_Encrypt( const std::string& in, std::string& out )
 {
-  EVP_MD_CTX mctx;
-  EVP_MD_CTX_init( &mctx );
+  auto mctx = EVP_MD_CTX_new()
 
-  EVP_DigestInit_ex( mctx, EVP_MD5(), nullptr );
+      EVP_DigestInit_ex( mctx, EVP_MD5(), nullptr );
   EVP_DigestUpdate( mctx, in.c_str(), in.length() );
 
   unsigned char hash[16];
