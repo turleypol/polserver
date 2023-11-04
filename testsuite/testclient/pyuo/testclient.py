0af887ae2477c901e5729722ad6ff6cc695e7ae8
turleypol <turley@polserver.com>
2023-11-04 23:11:33 +0100

    d6

--- testsuite/testclient/pyuo/testclient.py
+++ testsuite/testclient/pyuo/testclient.py
@@ -229,7 +229,7 @@ class PolServer:
     res={}
     res["id"]=ev.clientid
     res["type"]=ev.typestr()
-    if ev.type==Event.EVT_INIT or Event.EVT_EXIT:
+    if ev.type==Event.EVT_INIT or ev.type==Event.EVT_EXIT:
       pass
     elif (ev.type==Event.EVT_HP_CHANGED or
         ev.type==Event.EVT_MANA_CHANGED or
