3fdf9bfcac2f00c0d9069e608730abd1a64f06cd
turleypol <turley@polserver.com>
2023-12-31 14:42:23 +0100

    ic

--- cmake/Kaitai.cmake
+++ cmake/Kaitai.cmake
@@ -12,18 +12,18 @@ set(KAITAI_ARGS -DCMAKE_BUILD_TYPE=Release
    -DCMAKE_USER_MAKE_RULES_OVERRIDE_CXX=${CMAKE_CURRENT_LIST_DIR}/cxx_flag_overrides.cmake
    -DCMAKE_VERBOSE_MAKEFILE=ON
    -DCMAKE_OSX_ARCHITECTURES=${PIPED_OSX_ARCHITECTURES}
-   -DCMAKE_POLICY_DEFAULT_CMP0074=NEW
-   -DIconv_ROOT=${POL_EXT_LIB_DIR}
+   -DCMAKE_POLICY_DEFAULT_CMP0074=NEW # _ROOT vars can be set
+   -DCMAKE_DISABLE_FIND_PACKAGE_Iconv=TRUE # we dont want to link against
  )
 if (${linux})
   set(KAITAI_LIB "${KAITAI_INSTALL_DIR}/lib/libkaitai_struct_cpp_stl_runtime.a")
 else()
   set(KAITAI_LIB "${KAITAI_INSTALL_DIR}/lib/kaitai_struct_cpp_stl_runtime.lib")
   set(KAITAI_ARGS ${KAITAI_ARGS} 
-    -DZLIB_ROOT=${ZLIB_INSTALL_DIR}
+    -DZLIB_ROOT=${ZLIB_INSTALL_DIR} # own build zlib
     )
 endif()
-message("args ${KAITAI_ARGS}")
+
 ExternalProject_Add(kaitai_Ext
   SOURCE_DIR  ${KAITAI_SOURCE_DIR}
     PREFIX kaitai
@@ -50,8 +50,9 @@ add_library(kaitai STATIC IMPORTED)
 set_target_properties(kaitai PROPERTIES IMPORTED_LOCATION ${KAITAI_LIB})
 set_target_properties(kaitai PROPERTIES IMPORTED_IMPLIB ${KAITAI_LIB})
 
-file(MAKE_DIRECTORY ${KAITAI_INSTALL_DIR}/include)
+file(MAKE_DIRECTORY ${KAITAI_INSTALL_DIR}/include) #directory has to exists during configure
 set_target_properties(kaitai PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${KAITAI_INSTALL_DIR}/include)
+
 if (${windows})
   set_target_properties(kaitai PROPERTIES INTERFACE_LINK_LIBRARIES ${ZLIB_LIB})
 else()
