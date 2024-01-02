c2b0ef9a184d58000059d5e5a1198937268ec304
turleypol <turley@polserver.com>
2024-01-02 18:49:46 +0100

    tim3

--- pol-core/clib/Debugging/LogSink.cpp
+++ pol-core/clib/Debugging/LogSink.cpp
@@ -1,6 +1,8 @@
 #include "LogSink.h"
 
 #include <chrono>
+#include <fmt/chrono.h>
+#include <fmt/format.h>
 #include <time.h>
 
 #include "../clib.h"
@@ -20,16 +22,11 @@ void LogSink::addTimeStamp( std::ostream& stream )
 
 std::string LogSink::getTimeStamp()
 {
-  fmt::Writer writer;
   time_t tClockTime = system_clock::to_time_t( system_clock::now() );
   struct tm tTime = Clib::localtime( tClockTime );
 
   // write "[%m/%d %H:%M:%S] "
-  writer << '[' << fmt::pad( tTime.tm_mon + 1, 2, '0' ) << '/' << fmt::pad( tTime.tm_mday, 2, '0' )
-         << ' ' << fmt::pad( tTime.tm_hour, 2, '0' ) << ':' << fmt::pad( tTime.tm_min, 2, '0' )
-         << ':' << fmt::pad( tTime.tm_sec, 2, '0' ) << "] ";
-
-  return writer.str();
+  return fmt::format( "[{:%m/%d %T}] ", tTime );
 }
 
 }  // namespace Logging
