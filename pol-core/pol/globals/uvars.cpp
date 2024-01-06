29a1b7c3c822e7941fe439b83af176525522685c
turleypol <turley@polserver.com>
2024-01-06 11:36:26 +0100

    m

--- pol-core/pol/globals/uvars.cpp
+++ pol-core/pol/globals/uvars.cpp
@@ -216,7 +216,7 @@ void display_leftover_objects();
 
 void GameState::deinitialize()
 {
-  INFO_PRINT << "Initiating POL Cleanup....\n";
+  INFO_PRINT2( "Initiating POL Cleanup...." );
 
   networkManager.deinialize();
   deinit_ipc_vars();
