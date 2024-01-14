8c6886470c0c0b02ced582c31d52ff3fd20b9066
turleypol <turley@polserver.com>
2024-01-14 14:08:12 +0100

    j

--- cmake/core_tests.cmake
+++ cmake/core_tests.cmake
@@ -211,7 +211,7 @@ set_tests_properties( uoconvert_multis PROPERTIES FIXTURES_REQUIRED "client;shar
 set_tests_properties( uoconvert_multis PROPERTIES FIXTURES_SETUP uoconvert)
 
 add_test(NAME shard_ecompile
-  COMMAND ecompile -A -x
+  COMMAND ecompile -A -x -xt -l
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/coretest
 )
 set_tests_properties( shard_ecompile PROPERTIES FIXTURES_REQUIRED shard)
