4c012ccb5325880d280355e6e8035082b51241f3
turleypol <turley@polserver.com>
2024-01-15 14:08:10 +0100

    f

--- cmake/init.cmake
+++ cmake/init.cmake
@@ -243,7 +243,7 @@ macro(cmake_fake_target)
     cmake/Curl.cmake
     cmake/EscriptGrammarLib.cmake
     cmake/escript_grammar.cmake
-    cmake/Format.cmake
+    cmake/Format2.cmake
     cmake/Kaitai.cmake
     cmake/StackWalker.cmake
     cmake/TinyXML.cmake
