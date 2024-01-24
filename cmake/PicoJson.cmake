e8391d95f0a33db7c234b5d683ca89b931ba5797
turleypol <turley@polserver.com>
2024-01-24 06:54:03 +0100

    pico

--- cmake/PicoJson.cmake
+++ cmake/PicoJson.cmake
@@ -3,7 +3,7 @@
 
 # imported target to add include/lib dir and additional dependencies
 add_library(picojsonlib STATIC IMPORTED)
-set_target_properties(fmt PROPERTIES
+set_target_properties(picojsonlib PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/picojson-1.3.0"
   FOLDER 3rdParty
 )
