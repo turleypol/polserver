c7ff0d26a02c97bcb04146af047f1fcebfcdba8e
turleypol <turley@polserver.com>
2024-01-26 16:42:46 +0100

    tinyutf

--- cmake/UTF8.cmake
+++ cmake/UTF8.cmake
@@ -1,5 +1,5 @@
 # imported target to add include/lib dir and additional dependencies
 add_library(libutf8 INTERFACE IMPORTED)
 set_target_properties(libutf8 PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}"
+  INTERFACE_INCLUDE_DIRECTORIES "${POL_EXT_LIB_DIR}/utf8-2.3.4"
 )
