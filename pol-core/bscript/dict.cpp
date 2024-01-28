56b207f161cc5b73ecf9cef4f54989a5abc98fe9
turleypol <turley@polserver.com>
2024-01-28 21:19:48 +0100

    d

--- pol-core/bscript/dict.cpp
+++ pol-core/bscript/dict.cpp
@@ -232,7 +232,7 @@ BObjectImp* BDictionary::array_assign( BObjectImp* idx, BObjectImp* target, bool
 void BDictionary::addMember( const char* name, BObjectRef val )
 {
   BObject key( new String( name ) );
-  contents_[key] = std::move( val; )
+  contents_[key] = std::move( val );
 }
 
 void BDictionary::addMember( const char* name, BObjectImp* imp )
