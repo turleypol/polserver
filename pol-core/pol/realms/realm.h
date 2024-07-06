9e841889eaf13c46f248f6b8e9f8f36c5a6ff8f3
turleypol <turley@polserver.com>
2024-07-06 13:01:38 +0200

    more

--- pol-core/pol/realms/realm.h
+++ pol-core/pol/realms/realm.h
@@ -128,36 +128,11 @@ public:
 
   Multi::UMulti* find_supporting_multi( const Core::Pos3d& pos ) const;
 
-  bool lowest_standheight( unsigned short x, unsigned short y, short* z ) const  // TODO Pos
-  {
-    return lowest_standheight( Core::Pos2d( x, y ), z );
-  }
   bool lowest_standheight( const Core::Pos2d& pos, short* z ) const;
-  bool findstatic( unsigned short x, unsigned short y, unsigned short objtype ) const  // TODO Pos
-  {
-    return findstatic( Core::Pos2d( x, y ), objtype );
-  }
   bool findstatic( const Core::Pos2d& pos, unsigned short objtype ) const;
-  void getstatics( Plib::StaticEntryList& statics, unsigned short x, unsigned short y ) const
-  {
-    return getstatics( statics, Core::Pos2d( x, y ) );
-  }
   void getstatics( Plib::StaticEntryList& statics, const Core::Pos2d& pos ) const;
-  bool groundheight( unsigned short x, unsigned short y, short* z ) const  // TODO Pos
-  {
-    return groundheight( Core::Pos2d( x, y ), z );
-  }
   bool groundheight( const Core::Pos2d& pos, short* z ) const;
-  Plib::MAPTILE_CELL getmaptile( unsigned short x, unsigned short y ) const  // TODO Pos
-  {
-    return getmaptile( Core::Pos2d( x, y ) );
-  }
   Plib::MAPTILE_CELL getmaptile( const Core::Pos2d& pos ) const;
-  void getmapshapes( Plib::MapShapeList& shapes, unsigned short x, unsigned short y,
-                     unsigned int anyflags ) const  // TODO Pos
-  {
-    return getmapshapes( shapes, Core::Pos2d( x, y ), anyflags );
-  }
   void getmapshapes( Plib::MapShapeList& shapes, const Core::Pos2d& pos,
                      unsigned int anyflags ) const;
   void readmultis( Plib::MapShapeList& vec, unsigned short x, unsigned short y,
