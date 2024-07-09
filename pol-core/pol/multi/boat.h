d04ed6d2203e64e3bbd8203fd3d1284ecefa1656
turleypol <turley@polserver.com>
2024-07-09 13:22:19 +0200

    fit2

--- pol-core/pol/multi/boat.h
+++ pol-core/pol/multi/boat.h
@@ -164,7 +164,7 @@ public:
   virtual bool get_method_hook( const char* methodname, Bscript::Executor* ex,
                                 Core::ExportScript** hook, unsigned int* PC ) const override;
   bool navigable( const MultiDef& md, const Core::Pos4d& desired_pos ) const;
-  bool can_fit_at_location( const Core::Pos4d& desired_pos, const MultiDef* md ) const;
+  bool can_fit_at_location( const Core::Pos4d& desired_pos, const MultiDef* md = nullptr ) const;
   static bool objtype_passable( unsigned short graphic );
 
   virtual void on_color_changed() override;
