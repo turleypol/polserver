bbb5ccfd95bccd31080709897a1c6d06d0335dc6
turleypol <turley@polserver.com>
2024-08-07 14:13:39 +0200

    t5

--- pol-core/bscript/executor.inl.h
+++ pol-core/bscript/executor.inl.h
@@ -33,9 +33,12 @@ private:
 template <typename Callback>
 BObjectImp* Executor::makeContinuation( BObjectRef funcref, Callback callback, BObjectRefVec args )
 {
-  auto* func = funcref->impptr_if<BFunctionRef>();
-  if ( !func )
+  if ( !funcref->isa( BObjectImp::OTFuncRef ) )
+  {
     return new BError( "Invalid parameter type" );
+  }
+
+  auto func = funcref->impptr<BFunctionRef>();
 
   // Add function arguments to value stack. Add arguments if there are not enough.  Remove if
   // there are too many
