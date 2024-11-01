09016fff35cffdb55a39cc9d6a0c99e07db3a0e3
turleypol <turley@polserver.com>
2024-11-01 10:02:50 +0100

    logic

--- pol-core/pol/network/client.cpp
+++ pol-core/pol/network/client.cpp
@@ -750,10 +750,9 @@ void Client::set_update_range( u8 range )
   // limit range to defined min/max
   range = std::clamp( range, Core::settingsManager.ssopt.min_visual_range,
                       Core::settingsManager.ssopt.max_visual_range );
-
-  // remove all objects, but not if its the first pkt
+  // delete/send all objects, but not if its the first pkt
   if ( old_range != range && gd->original_client_update_range != 0 )
-    Core::remove_objects_inrange( this );
+    chr->update_objects_on_range_change( range );
 
   gd->update_range = range;
 
@@ -765,14 +764,6 @@ void Client::set_update_range( u8 range )
   {
     // update global updaterange (maximum multi radius/client view range)
     Core::gamestate.update_range_from_client( range );
-    if ( gd->original_client_update_range != 0 )
-    {
-      // reset lastpos so that everything counts as newly inrange
-      Core::Pos4d pos = chr->lastpos;
-      chr->lastpos = Core::Pos4d{};
-      Core::send_objects_newly_inrange( this );
-      chr->lastpos = pos;
-    }
   }
 }
 
