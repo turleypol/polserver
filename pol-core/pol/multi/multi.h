5e9dfeddff2e6c9fefc707563c6a9c530f5f247c
turleypol <turley@polserver.com>
2024-11-10 16:30:28 +0100

    r5

--- pol-core/pol/multi/multi.h
+++ pol-core/pol/multi/multi.h
@@ -92,6 +92,7 @@ public:
   virtual class UHouse* as_house();
   virtual Bscript::BObjectImp* make_ref() override;
   virtual Bscript::BObjectImp* get_script_member_id( const int id ) const override;
+  using Items::Item::set_script_member_id;
   virtual Bscript::BObjectImp* set_script_member_id( const int id, int value ) override;
   virtual bool get_method_hook( const char* methodname, Bscript::Executor* ex,
                                 Core::ExportScript** hook, unsigned int* PC ) const override;
