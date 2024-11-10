4c9526cefa3bfb6d871a8f52ee1a41a8782ec047
turleypol <turley@polserver.com>
2024-11-10 20:12:48 +0100

    f7

--- pol-core/pol/regions/guardrgn.cpp
+++ pol-core/pol/regions/guardrgn.cpp
@@ -24,7 +24,7 @@ JusticeRegion::JusticeRegion( Clib::ConfigElem& elem, RegionId id )
     : Region( elem, id ),
       guarded_( elem.remove_bool( "Guarded", false ) ),
       nocombat_( elem.remove_bool( "NoCombat", false ) ),
-      nodecay_( elem.remove_string( "NoDecay", false ) ),
+      nodecay_( elem.remove_bool( "NoDecay", false ) ),
       region_name_( elem.rest() ),
       entertext_( elem.remove_string( "EnterText", "" ) ),
       leavetext_( elem.remove_string( "LeaveText", "" ) ),
