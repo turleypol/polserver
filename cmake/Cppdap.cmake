5db6f3915b28d6d54d8c3dec312fc2ef6a9a6166
turleypol <turley@polserver.com>
2025-01-05 15:52:39 +0100

    cppdaptest

--- cmake/Cppdap.cmake
+++ cmake/Cppdap.cmake
@@ -26,9 +26,9 @@ set(CPPDAP_ARGS ${CPPDAP_FLAGS}
 
 if(NOT EXISTS ${CPPDAP_LIB})
   ExternalProject_Add(libcppdap_ext
-    GIT_REPOSITORY   ${CPPDAP_REPOSITORY}
-    GIT_TAG          ${CPPDAP_VERSION_REF}
-    GIT_SHALLOW      TRUE
+    GIT_REPOSITORY   "../lib/cppdap1" #${CPPDAP_REPOSITORY}
+    #    GIT_TAG          ${CPPDAP_VERSION_REF}
+    #    GIT_SHALLOW      TRUE
     PREFIX           cppdap
     LIST_SEPARATOR |
     CMAKE_ARGS       ${CPPDAP_ARGS}
