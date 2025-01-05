0b56e4b0d80d83ee4bef032ce1a584ad034c9f79
turleypol <turley@polserver.com>
2025-01-05 13:13:08 +0100

    u

--- cmake/Boost.cmake
+++ cmake/Boost.cmake
@@ -51,7 +51,8 @@ set(boost_needs_extract FALSE)
 if (NOT EXISTS "${BOOST_SOURCE_DIR}/boost")
   message("  - will extract")
   ExternalProject_Add(libboost_ext
-    URL "https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83_0.tar.bz2"
+    URL "https://archives.boost.io/release/1.83.0/source/boost_1_83_0.tar.bz2"
+    URL_HASH SHA256=6478edfe2f3305127cffe8caf73ea0176c53769f4bf1585be237eb30798c3b8e
     SOURCE_DIR "${BOOST_SOURCE_DIR}"
     CONFIGURE_COMMAND "" #${BOOST_CONFIGURE_COMMAND} --with-toolset=${BOOST_TOOLSET}
     BUILD_COMMAND ""
