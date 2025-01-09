ac133ee5692b9ff71d1c8f4704b473ad0ac46fae
turleypol <turley@polserver.com>
2025-01-09 22:11:56 +0100

    trace11

--- pol-core/pol/savedata.cpp
+++ pol-core/pol/savedata.cpp
@@ -161,7 +161,7 @@ SaveContext::SaveContext()
   party.comment( "\n" );
 }
 
-SaveContext::~SaveContext()
+SaveContext::~SaveContext() noexcept( false )
 {
   pol.flush_file();
   objects.flush_file();
