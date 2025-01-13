b567a3b0a4f3f637951241f54f4eeaadf4e1887c
turleypol <turley@polserver.com>
2025-01-13 17:37:48 +0100

    stack3

--- pol-core/clib/Debugging/ExceptionParser.cpp
+++ pol-core/clib/Debugging/ExceptionParser.cpp
@@ -552,6 +552,9 @@ string ExceptionParser::getTrace()
   auto stack = boost::stacktrace::stacktrace::from_current_exception();
   if ( stack.empty() )
     stack = boost::stacktrace::stacktrace();
+  for ( const auto f : stack )
+    POLLOG_ERROR( "name:{} addr:{} file:{} line:{}", f.name(), f.address(), f.source_file(),
+                  f.source_line() );
   return boost::stacktrace::to_string( stack );
 }
 
