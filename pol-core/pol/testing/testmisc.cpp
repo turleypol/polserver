6570991223100490a515c9ce7df2fd032f3d1a9d
turleypol <turley@polserver.com>
2025-01-18 12:01:59 +0100

    clamp2

--- pol-core/pol/testing/testmisc.cpp
+++ pol-core/pol/testing/testmisc.cpp
@@ -379,6 +379,8 @@ void clamp_test()
       return []() { Clib::clamp_convert<s16>( (u16)0xFFFF ); }, 0x3fff, "u16->s16" );
   UnitTest(
       return []() { Clib::clamp_convert<u16>( (s16)-1 ); }, 0, "s16->u16" );
+  UnitTest(
+      return []() { Clib::clamp_convert<u8>( (u16)0xffff ); }, 0xff, "u16->u8" );
 }
 }  // namespace Testing
 }  // namespace Pol
