0837bb60121ea601e4f3babb1a298c5f06fddabd
turleypol <turley@polserver.com>
2025-01-20 13:48:28 +0100

    t4

--- pol-core/pol/testing/testmisc.cpp
+++ pol-core/pol/testing/testmisc.cpp
@@ -411,15 +411,15 @@ void clamp_test()
             std::numeric_limits<s8>::min(), "s32 min->s8" );
   UnitTest( []() { return Clib::clamp_convert<s8>( std::numeric_limits<s64>::min() ); },
             std::numeric_limits<s8>::min(), "s64 min->s8" );
-#define T_MIN_MIN( to, from )                                                                  \
-  UnitTest( []() { return Clib::clamp_convert<to>( std::numeric_limits<from>::min() ); },      \
-            std::numeric_limits<to>::min(),                                                    \
-            fmt::format( #from " {:#x} ->" #to " = {:#x}", std::numerical_limits<from>::min(), \
+#define T_MIN_MIN( to, from )                                                                \
+  UnitTest( []() { return Clib::clamp_convert<to>( std::numeric_limits<from>::min() ); },    \
+            std::numeric_limits<to>::min(),                                                  \
+            fmt::format( #from " {:#x} ->" #to " = {:#x}", std::numeric_limits<from>::min(), \
                          std::numeric_limits<to> min() ) )
-#define T_MAX_MAX( to, from )                                                                  \
-  UnitTest( []() { return Clib::clamp_convert<to>( std::numeric_limits<from>::max() ); },      \
-            std::numeric_limits<to>::max(),                                                    \
-            fmt::format( #from " {:#x} ->" #to " = {:#x}", std::numerical_limits<from>::max(), \
+#define T_MAX_MAX( to, from )                                                                \
+  UnitTest( []() { return Clib::clamp_convert<to>( std::numeric_limits<from>::max() ); },    \
+            std::numeric_limits<to>::max(),                                                  \
+            fmt::format( #from " {:#x} ->" #to " = {:#x}", std::numeric_limits<from>::max(), \
                          std::numeric_limits<to> max() ) )
 #define T_ZERO_MIN( to, from )                                                      \
   UnitTest(                                                                         \
