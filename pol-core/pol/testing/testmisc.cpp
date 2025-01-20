4f8c49e12d42b819108d77cdc97373b57be4df75
turleypol <turley@polserver.com>
2025-01-20 20:08:29 +0100

    unsigned

--- pol-core/pol/testing/testmisc.cpp
+++ pol-core/pol/testing/testmisc.cpp
@@ -380,7 +380,7 @@ void clamp_test()
   // "from" start == "to" res
 #define T_CLAMP( from, start, to, res )                             \
   UnitTest( []() { return Clib::clamp_convert<to>( start ); }, res, \
-            fmt::format( " {:#x} " #from " == {:#x} " #to, start, res ) )
+            fmt::format( " {:#x} " #from " -> {:#x} " #to, start, res ) )
 
 #define LMIN( T ) std::numeric_limits<T>::min()
 #define LMAX( T ) std::numeric_limits<T>::max()
@@ -461,6 +461,81 @@ void clamp_test()
   T_CLAMP( u32, LMAX( u32 ), s64, (s64)LMAX( u32 ) );
   T_CLAMP( u64, LMAX( u64 ), s64, LMAX( s64 ) );
 
+  // u8 combinations
+  T_CLAMP( s8, LMIN( s8 ), u8, 0u );
+  T_CLAMP( s16, LMIN( s16 ), u8, 0u );
+  T_CLAMP( s32, LMIN( s32 ), u8, 0u );
+  T_CLAMP( s64, LMIN( s64 ), u8, 0u );
+  T_CLAMP( s8, LMAX( s8 ), u8, (u8)LMAX( s8 ) );
+  T_CLAMP( s16, LMAX( s16 ), u8, LMAX( u8 ) );
+  T_CLAMP( s32, LMAX( s32 ), u8, LMAX( u8 ) );
+  T_CLAMP( s64, LMAX( s64 ), u8, LMAX( u8 ) );
+
+  T_CLAMP( u8, LMIN( u8 ), u8, 0u );
+  T_CLAMP( u16, LMIN( u16 ), u8, 0u );
+  T_CLAMP( u32, LMIN( u32 ), u8, 0u );
+  T_CLAMP( u64, LMIN( u64 ), u8, 0u );
+  T_CLAMP( u8, LMAX( u8 ), u8, LMAX( u8 ) );
+  T_CLAMP( u16, LMAX( u16 ), u8, LMAX( u8 ) );
+  T_CLAMP( u32, LMAX( u32 ), u8, LMAX( u8 ) );
+  T_CLAMP( u64, LMAX( u64 ), u8, LMAX( u8 ) );
+
+  // u16 combinations
+  T_CLAMP( s8, LMIN( s8 ), u16, 0u );
+  T_CLAMP( s16, LMIN( s16 ), u16, 0u );
+  T_CLAMP( s32, LMIN( s32 ), u16, 0u );
+  T_CLAMP( s64, LMIN( s64 ), u16, 0u );
+  T_CLAMP( s8, LMAX( s8 ), u16, (u16)LMAX( s8 ) );
+  T_CLAMP( s16, LMAX( s16 ), u16, (u16)LMAX( s16 ) );
+  T_CLAMP( s32, LMAX( s32 ), u16, LMAX( u16 ) );
+  T_CLAMP( s64, LMAX( s64 ), u16, LMAX( u16 ) );
+
+  T_CLAMP( u8, LMIN( u8 ), u16, 0u );
+  T_CLAMP( u16, LMIN( u16 ), u16, 0u );
+  T_CLAMP( u32, LMIN( u32 ), u16, 0u );
+  T_CLAMP( u64, LMIN( u64 ), u16, 0u );
+  T_CLAMP( u8, LMAX( u8 ), u16, LMAX( u8 ) );
+  T_CLAMP( u16, LMAX( u16 ), u16, LMAX( u16 ) );
+  T_CLAMP( u32, LMAX( u32 ), u16, LMAX( u16 ) );
+  T_CLAMP( u64, LMAX( u64 ), u16, LMAX( u16 ) );
+
+  // u32 combinations
+  T_CLAMP( s8, LMIN( s8 ), u32, 0u );
+  T_CLAMP( s16, LMIN( s16 ), u32, 0u );
+  T_CLAMP( s32, LMIN( s32 ), u32, 0u );
+  T_CLAMP( s64, LMIN( s64 ), u32, 0u );
+  T_CLAMP( s8, LMAX( s8 ), u32, (u32)LMAX( s8 ) );
+  T_CLAMP( s16, LMAX( s16 ), u32, (u32)LMAX( s16 ) );
+  T_CLAMP( s32, LMAX( s32 ), u32, (u32)LMAX( s32 ) );
+  T_CLAMP( s64, LMAX( s64 ), u32, LMAX( u32 ) );
+
+  T_CLAMP( u8, LMIN( u8 ), u32, 0u );
+  T_CLAMP( u16, LMIN( u16 ), u32, 0u );
+  T_CLAMP( u32, LMIN( u32 ), u32, 0u );
+  T_CLAMP( u64, LMIN( u64 ), u32, 0u );
+  T_CLAMP( u8, LMAX( u8 ), u32, LMAX( u8 ) );
+  T_CLAMP( u16, LMAX( u16 ), u32, LMAX( u16 ) );
+  T_CLAMP( u32, LMAX( u32 ), u32, LMAX( u32 ) );
+  T_CLAMP( u64, LMAX( u64 ), u32, LMAX( u32 ) );
+
+  // u64 combinations
+  T_CLAMP( s8, LMIN( s8 ), u64, 0u );
+  T_CLAMP( s16, LMIN( s16 ), u64, 0u );
+  T_CLAMP( s32, LMIN( s32 ), u64, 0u );
+  T_CLAMP( s64, LMIN( s64 ), u64, 0u );
+  T_CLAMP( s8, LMAX( s8 ), u64, (u64)LMAX( s8 ) );
+  T_CLAMP( s16, LMAX( s16 ), u64, (u64)LMAX( s16 ) );
+  T_CLAMP( s32, LMAX( s32 ), u64, (u64)LMAX( s32 ) );
+  T_CLAMP( s64, LMAX( s64 ), u64, (u64)LMAX( s64 ) );
+
+  T_CLAMP( u8, LMIN( u8 ), u64, 0u );
+  T_CLAMP( u16, LMIN( u16 ), u64, 0u );
+  T_CLAMP( u32, LMIN( u32 ), u64, 0u );
+  T_CLAMP( u64, LMIN( u64 ), u64, 0u );
+  T_CLAMP( u8, LMAX( u8 ), u64, LMAX( u8 ) );
+  T_CLAMP( u16, LMAX( u16 ), u64, LMAX( u16 ) );
+  T_CLAMP( u32, LMAX( u32 ), u64, LMAX( u32 ) );
+  T_CLAMP( u64, LMAX( u64 ), u64, LMAX( u64 ) );
 #undef T_CLAMP
 #undef LMAX
 #undef LMIN
