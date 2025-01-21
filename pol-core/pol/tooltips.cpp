0910f6a21bf70ed9262dd5f7665b33fcea81c175
turleypol <turley@polserver.com>
2025-01-21 21:52:51 +0100

    f

--- pol-core/pol/tooltips.cpp
+++ pol-core/pol/tooltips.cpp
@@ -66,7 +66,7 @@ void handle_request_tooltip( Network::Client* client, PKTIN_B6* msgin )
 // 0xD6
 void send_object_cache( Network::Client* client, const UObject* obj )
 {
-  if ( settingsManager.ssopt.uo_feature_enable & Plib::A9Feature::AOS == Plib::A9Feature::AOS )
+  if ( ( settingsManager.ssopt.uo_feature_enable & Plib::A9Feature::AOS == ) Plib::A9Feature::AOS )
   {
     auto pkt_rev = Network::ObjRevisionPkt( obj->serial_ext, obj->rev() );
     pkt_rev.Send( client );
@@ -75,7 +75,7 @@ void send_object_cache( Network::Client* client, const UObject* obj )
 
 void send_object_cache_to_inrange( const UObject* obj )
 {
-  if ( settingsManager.ssopt.uo_feature_enable & Plib::A9Feature::AOS == Plib::A9Feature::AOS )
+  if ( ( settingsManager.ssopt.uo_feature_enable & Plib::A9Feature::AOS ) == Plib::A9Feature::AOS )
   {
     auto pkt_rev = Network::ObjRevisionPkt( obj->serial_ext, obj->rev() );
 
