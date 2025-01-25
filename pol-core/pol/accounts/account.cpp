a71b4d604961732cc1d49f7a097841da09e976a2
turleypol <turley@polserver.com>
2025-01-25 15:16:13 +0100

    store1⁷

--- pol-core/pol/accounts/account.cpp
+++ pol-core/pol/accounts/account.cpp
@@ -64,8 +64,7 @@ void Account::readfrom( Clib::ConfigElem& elem )
   enabled_ = elem.remove_bool( "ENABLED", true );
   banned_ = elem.remove_bool( "BANNED", false );
   auto exp = elem.remove_string(
-      "UOExpansion",
-      Plib::getExpansionName( Core::settingsManager.ssopt.expansion.Expansion() ).c_str() );
+      "UOExpansion", Plib::getExpansionName( Core::settingsManager.ssopt.expansion.Expansion() ) );
   uo_expansion_ = convert_uo_expansion( exp );
   expansion_ = Plib::AccountExpansion(
       exp, Core::settingsManager.ssopt.expansion.extensionFlags() );  // store flags?
