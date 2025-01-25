28ef93045a2e2a94651a22ea174304bb73db90e2
turleypol <turley@polserver.com>
2025-01-25 15:32:45 +0100

    store19

--- pol-core/plib/uoexpansion.cpp
+++ pol-core/plib/uoexpansion.cpp
@@ -4,8 +4,8 @@ namespace Pol
 {
 namespace Plib
 {
-constexpr std::array<std::string_view, numExpansions> ExpansionNames = {
-    "T2A", "LBR", "AOS", "SE", "ML", "KR", "SA", "HSA" };
+constexpr std::array<std::string, numExpansions> ExpansionNames = { "T2A", "LBR", "AOS", "SE",
+                                                                    "ML",  "KR",  "SA",  "HSA" };
 
 static_assert( B9Feature::DefaultT2A == (B9Feature)0x1 );
 static_assert( B9Feature::DefaultLBR == (B9Feature)0x2 );
