ab416632f209f73c4db578c9c330a1d7586617ed
turleypol <turley@polserver.com>
2025-01-25 14:07:27 +0100

    store10

--- pol-core/pol/ssopt.cpp
+++ pol-core/pol/ssopt.cpp
@@ -83,9 +83,10 @@ void ServSpecOpt::read_servspecopt()
   settingsManager.ssopt.hidden_turns_count = elem.remove_bool( "HiddenTurnsCount", true );
   settingsManager.ssopt.invul_tag = elem.remove_ushort( "InvulTag", 1 );
   auto a9flag = static_cast<Plib::A9Feature>( elem.remove_ushort( "UOFeatureEnable", 0 ) );
-  auto default_expansion =  elem.remove_string( "DefaultExpansion", Plib::getExpansionName(Plib::ExpansionVersion::T2A );
+  auto default_expansion = elem.remove_string(
+      "DefaultExpansion", Plib::getExpansionName( Plib::ExpansionVersion::T2A ).c_str() );  // new
   settingsManager.ssopt.uo_feature_enable = a9flag;
-  settingsManager.ssopt.expansion = Plib::ServerExpansion(a9flag,default_expansion);
+  settingsManager.ssopt.expansion = Plib::ServerExpansion( a9flag, default_expansion );
   settingsManager.ssopt.starting_gold = elem.remove_ushort( "StartingGold", 100 );
   settingsManager.ssopt.item_color_mask = elem.remove_ushort( "ItemColorMask", 0xFFF );
   settingsManager.ssopt.use_win_lfh = elem.remove_bool( "UseWinLFH", false );
