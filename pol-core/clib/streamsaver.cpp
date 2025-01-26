64a168b991bbef64af8e96740e8722b7eaad42ac
turleypol <turley@polserver.com>
2025-01-26 20:36:22 +0100

    clean

--- pol-core/clib/streamsaver.cpp
+++ pol-core/clib/streamsaver.cpp
@@ -1,32 +1,24 @@
 #include <exception>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <string>
 
 #include "streamsaver.h"
 
-namespace Pol
+namespace Pol::Clib
 {
-namespace Clib
-{
+StreamWriter::StreamWriter( std::ostream& stream ) : _stream( stream ) {}
 
-StreamWriter::StreamWriter( std::ofstream* stream ) : _stream( stream )
-#if 0
-      _fs_time( 0 ),
-#endif
+void StreamWriter::open_fstream( const std::string& filepath, std::ofstream& s )
 {
-}
-
-void StreamWriter::init( const std::string& filepath )
-{
-  _stream->exceptions( std::ios_base::failbit | std::ios_base::badbit );
-  _stream->open( filepath.c_str(), std::ios::out | std::ios::trunc );
+  s.exceptions( std::ios_base::failbit | std::ios_base::badbit );
+  s.open( filepath, std::ios::out | std::ios::trunc );
 }
 
 void StreamWriter::flush_file()
 {
-  _stream->flush();
+  _stream.flush();
 }
 
-}  // namespace Clib
-}  // namespace Pol
+}  // namespace Pol::Clib
--- pol-core/clib/streamsaver.h
+++ pol-core/clib/streamsaver.h
@@ -1,5 +1,4 @@
-#ifndef CLIB_STREAMSAVER_H
-#define CLIB_STREAMSAVER_H
+#pragma once
 
 #include <fmt/format.h>
 #include <fstream>
@@ -8,18 +7,12 @@
 #include <string>
 #include <type_traits>
 
-#if 0
-#include "timer.h"
-#endif
-
-namespace Pol
-{
-namespace Clib
+namespace Pol::Clib
 {
 class StreamWriter
 {
 public:
-  StreamWriter( std::ofstream* stream );
+  StreamWriter( std::ofstream stream );
   ~StreamWriter() = default;
   StreamWriter( const StreamWriter& ) = delete;
   StreamWriter& operator=( const StreamWriter& ) = delete;
@@ -28,41 +21,36 @@ public:
   void add( Str&& key, T&& value )
   {
     if constexpr ( !std::is_same<std::decay_t<T>, bool>::value )  // force bool to write as 0/1
-      fmt::print( *_stream, "\t{}\t{}\n", key, value );
+      fmt::print( _stream, "\t{}\t{}\n", key, value );
     else
-      fmt::print( *_stream, "\t{}\t{:d}\n", key, value );
+      fmt::print( _stream, "\t{}\t{:d}\n", key, value );
   }
   template <typename Str, typename... Args>
   void comment( Str&& format, Args&&... args )
   {
-    *_stream << "# ";
+    _stream << "# ";
     if constexpr ( sizeof...( args ) == 0 )
-      *_stream << format;
+      _stream << format;
     else
-      fmt::print( *_stream, format, args... );
-    *_stream << '\n';
+      fmt::print( _stream, format, args... );
+    _stream << '\n';
   }
   template <typename Str>
   void begin( Str&& key )
   {
-    fmt::print( *_stream, "{}\n{{\n", key );
+    fmt::print( _stream, "{}\n{{\n", key );
   }
   template <typename Str, typename StrValue>
   void begin( Str&& key, StrValue&& value )
   {
-    fmt::print( *_stream, "{} {}\n{{\n", key, value );
+    fmt::print( _stream, "{} {}\n{{\n", key, value );
   }
-  void end() { *_stream << "}\n\n"; }
-  void init( const std::string& filepath );
+  void end() { _stream << "}\n\n"; }
+  void open_fstream( const std::string& filepath, std::ofstream& s );
   void flush_file();
 
 protected:
-  std::ofstream* _stream;
-#if 0
-      Tools::HighPerfTimer::time_mu _fs_time;
-#endif
+  std::ofstream& _stream;
 };
 
-}  // namespace Clib
-}  // namespace Pol
-#endif  // CLIB_STREAMSAVER_H
+}  // namespace Pol::Clib
