f3afffb6148e2a4975a40a5b2e90e87a91b80855
turleypol <turley@polserver.com>
2025-01-27 08:14:36 +0100

    buffincrease

--- pol-core/clib/streamsaver.cpp
+++ pol-core/clib/streamsaver.cpp
@@ -17,10 +17,10 @@ StreamWriter::~StreamWriter() noexcept( false )
 
 void StreamWriter::open_fstream( const std::string& filepath, std::ofstream& s )
 {
-  _buf.reset( new char[1024 * 1024] );
+  _buf.reset( new char[1024 * 1024 * 1024] );
   s.exceptions( std::ios_base::failbit | std::ios_base::badbit );
   s.open( filepath, std::ios::out | std::ios::trunc );
-  s.rdbuf()->pubsetbuf( _buf.get(), 1024 * 1024 );
+  s.rdbuf()->pubsetbuf( _buf.get(), 1024 * 1024 * 1024 );
 }
 
 void StreamWriter::flush_file()
