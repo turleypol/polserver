6b82fee539e3156f1f4f0020be1e141a97abdda1
turleypol <turley@polserver.com>
2025-02-18 09:01:48 +0100

    test

--- cmake/compile_defs.cmake
+++ cmake/compile_defs.cmake
@@ -143,6 +143,9 @@ function(set_compile_flags target is_executable)
         -OPT:REF # remove unused blocks
       )
     endif()
+    if (${is_executable})
+      set_target_properties(${target} PROPERTIES LINK_FLAGS "/PDBALTPATH:${target}.pdb")
+    endif()
   endif()
 
   if(${linux})
