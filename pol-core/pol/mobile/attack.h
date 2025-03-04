dd88a984121a209c5a194c1f0f98a592c44d39f2
turleypol <turley@polserver.com>
2025-03-04 07:13:46 +0100

    start3

--- pol-core/pol/mobile/attack.h
+++ pol-core/pol/mobile/attack.h
@@ -16,29 +16,15 @@ class Attackable
 {
 public:
   Attackable() = default;
-  Attackable( Character* chr ) : _opp( chr ){};
-  Attackable( Items::Item* item ) : _opp( item )
-  {
-    if ( !item->is_attackable() )
-      _opp = nullptr;
-  };
+  Attackable( Character* chr );
+  Attackable( Items::Item* item );
   explicit operator bool() const { return _opp != nullptr; };
   void clear() { _opp = nullptr; };
   UObject* object() { return _opp; };
-  Character* mobile()
-  {
-    if ( valid() && _opp->ismobile() )
-      return static_cast<Character*>( _opp );
-    return nullptr;
-  };
-  Items::Item* item()
-  {
-    if ( valid() && _opp->isitem() )
-      return static_cast<Items::Item*>( _opp );
-    return nullptr;
-  };
+  Character* mobile();
+  Items::Item* item();
 
 private:
   UObject* _opp = nullptr;
-}
+};
 }  // namespace Pol::Mobile
