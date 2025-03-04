3f0c0cfa0ae34845d4879bf138b1e943346a3820
turleypol <turley@polserver.com>
2025-03-04 15:19:49 +0100

    oppof7

--- pol-core/pol/repsys.cpp
+++ pol-core/pol/repsys.cpp
@@ -193,19 +193,20 @@ void RepSystem::repsys_task( Mobile::Character* amy )
   // the 'defensive hostiles' are those that are attacking Amy while she's not innocent.
   // If she's innocent at the end of this, they will break off the attack.
   THREAD_CHECKPOINT( tasks, 1001 );
-  Mobile::Character::CharacterSet defensive_hostiles;
-  for ( Mobile::Character::CharacterSet::iterator h_itr = amy->opponent_of.begin();
-        h_itr != amy->opponent_of.end(); ++h_itr )
+  std::setMobile::Character* > defensive_hostiles;
+  for ( const auto& opp : amy->opponent_of )
   {
-    Mobile::Character* hostile_bob = *h_itr;
-    THREAD_CHECKPOINT( tasks, 1002 );
-
-    if ( !amy->is_innocent_to( hostile_bob ) )
+    if ( auto* bob = opp.mobile() )
     {
-      THREAD_CHECKPOINT( tasks, 1003 );
-      defensive_hostiles.insert( hostile_bob );
+      THREAD_CHECKPOINT( tasks, 1002 );
+
+      if ( !amy->is_innocent_to( bob ) )
+      {
+        THREAD_CHECKPOINT( tasks, 1003 );
+        defensive_hostiles.insert( bob );
+      }
+      THREAD_CHECKPOINT( tasks, 1004 );
     }
-    THREAD_CHECKPOINT( tasks, 1004 );
   }
 
 
@@ -305,10 +306,8 @@ void RepSystem::repsys_task( Mobile::Character* amy )
   THREAD_CHECKPOINT( tasks, 1025 );
   // For those that were hostile to Amy due to her non-Innocence, if she's Innocent now,
   // break off the attack.
-  for ( Mobile::Character::CharacterSet::iterator h_itr = defensive_hostiles.begin();
-        h_itr != defensive_hostiles.end(); ++h_itr )
+  for ( auto* bob : defensive_hostiles )
   {
-    Mobile::Character* bob = *h_itr;
     THREAD_CHECKPOINT( tasks, 1026 );
 
     if ( amy->is_innocent_to( bob ) )
