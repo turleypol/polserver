691d50c67456756bbb5e7c111ed8c77198df6a8d
turleypol <turley@polserver.com>
2025-07-23 10:37:54 +0200

    2010

--- pol-core/clib/logfacility.h
+++ pol-core/clib/logfacility.h
@@ -180,9 +180,9 @@ struct Message
     if constexpr ( sizeof...( args ) == 0 )
     {
       if constexpr ( newline )
-        send( std::string{ format.str } + '\n' );
+        send( std::string{ format.str.data(), format.str.size() } + '\n' );
       else
-        send( std::string{ format.str } );
+        send( std::string{ format.str.data(), format.str.size() } );
     }
     else
     {
