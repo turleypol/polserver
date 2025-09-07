83b8aefbe2969e0166acc355e99fcb2901d590c5
turleypol <turley@polserver.com>
2025-09-07 14:42:02 +0200

    sync6

--- testsuite/pol/summary.py
+++ testsuite/pol/summary.py
@@ -1,5 +1,11 @@
 import re
+import os
+import sys
 
+print("# Pol test summary")
+if not os.path.exists("log/pol.log"):
+    print("no testrun")
+    sys.exit(0)
 content = None
 with open("log/pol.log", "r") as file:
     content = file.readlines()
@@ -13,10 +19,12 @@ for line in content:
     m=re.match(r"\[.*\] (\w+) \(testpkgs/(\w+)/\)", line)
     if m is not None:
         active_pkg = m.group(2)
+        print("|", active_pkg,"|","|","|","|","|","|")
         continue
     m=re.match(r"\[.*\]   Calling (\w+).ecl", line)
     if m is not None:
         active_script = m.group(1)
+        print("|", "|",active_script,"|","|","|","|","|")
         continue
     m=re.match(r"\[.*\]     Calling (\w+)\.\.", line)
     if m is not None:
@@ -28,8 +36,10 @@ for line in content:
         continue
     m=re.match(r"\[.*\]     \.\.(.*)ms", line)
     if m is not None:
+        print("|", "|","|",active_function,"|", failed if failed is not None else "ok", "|",m.group(1)+"ms","|",lines if failed is not None else '',"|")
         end = m.group(1)
-        print(f"- {active_pkg}/{active_script} : {active_function} {failed if failed is not None else 'ok'} {end}ms")
+        #print(f"- {active_pkg}/{active_script} : {active_function} {failed if failed is not None else 'ok'} {end}ms")
         failed = None
+        lines=""
     else:
-        lines+=line.rstrip()+"\n"
+        lines+=line.rstrip()+"  \n"
