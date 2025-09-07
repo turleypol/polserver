ad49397acf188382f344310d8ffc5c6d3af84274
turleypol <turley@polserver.com>
2025-09-07 21:30:39 +0200

    clean

--- testsuite/pol/summary.py
+++ testsuite/pol/summary.py
@@ -5,54 +5,73 @@ import sys
 if not os.path.exists("log/pol.log"):
     print("no testrun")
     sys.exit(0)
-content = None
 with open("log/pol.log", "r") as file:
     content = file.readlines()
-failed=None
 lines=""
 tests=0
 fails=0
 output="|pkg|script|function|result|duration|output|\n"
 output+="|-|-|-|-|-|-|\n"
+res=[]
+class Test:
+    def __init__(self,name):
+        self.name=name
+        self.dur=""
+        self.result = ":white_check_mark:"
+        self.output=""
+        self.sub=[]
+
+curpkg=None
+curscript=None
+curfunc=None
 for line in content:
     line = line.rstrip()
     m=re.match(r"\[.*\] (\w+) \(testpkgs/(\w+)/\)", line)
     if m is not None:
-        active_pkg = m.group(2)
-        output+=f"|{active_pkg}| | | | | |\n"
+        res.append(Test(m.group(2)))
+        curpkg=res[-1]
         continue
-    m=re.match(r"\[.*\]   Calling (\w+).ecl", line)
+    m=re.match(r"\[.*\]\s+Calling (\w+).ecl", line)
     if m is not None:
-        active_script = m.group(1)
-        output+=f"| |{active_script}| | | | |\n"
+        if "cleanup" == m.group(1):
+            continue
+        curpkg.sub.append(Test(m.group(1)))
+        curscript = curpkg.sub[-1]
         continue
-    m=re.match(r"\[.*\]     Calling (\w+)\.\.", line)
+    m=re.match(r"\[.*\]\s+Calling (\w+)\.\.", line)
     if m is not None:
-        active_function = m.group(1)
+        curscript.sub.append(Test(m.group(1)))
+        curfunc = curscript.sub[-1]
         lines=""
         continue
-    m=re.match(r"\[.*\]       failed: (.*)", line)
+    m=re.match(r"\[.*\]\s+failed: (.*)", line)
     if m is not None:
-        failed = m.group(1)
-        dur="-"
+        curfunc.result = ":x:"
+        curfunc.output = lines + m.group(1)
+        curfunc.dur = "-"
+        curscript.result = ":x:"
+        curpkg.result = ":x:"
         fails+=1
-        fstr = ":x:"
-        fout = lines+failed
     else: 
-        m=re.match(r"\[.*\]     \.\.(.*)ms", line)
+        m=re.match(r"\[.*\]\s+\.\.(.*)ms", line)
         if m is not None:
-            dur = m.group(1)+"ms"
-            fstr = ":white_check_mark:"
-            fout = ""
+            if not curfunc.dur:
+                curfunc.dur = m.group(1)+"ms"
+            elif not curscript.dur:
+                curscript.dur = m.group(1)+"ms"
+            elif not curpkg.dur:
+                curpkg.dur = m.group(1)+"ms"
     if m is not None:
         tests+=1
-        output+=f"| | |{active_function}|{fstr}|{dur}|{fout}|\n"
-        failed = None
         lines=""
     else:
         lines+=line+"<br/>"
-
-
+for r in res:
+    output+=f"|{r.name}| | |{r.result}|{r.dur}|{r.output}|\n"
+    for s in r.sub:
+        output+=f"| |{s.name}| |{s.result}|{s.dur}|{s.output}|\n"
+        for f in s.sub:
+            output+=f"| | |{f.name}|{f.result}|{f.dur}|{f.output}|\n"
 print(f"<details><summary>{fails} tests failed out of {tests}</summary>")
 print("")
 print(output)
