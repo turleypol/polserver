c612bafc03720938b037518c3b827d40bfea10de
turleypol <turley@polserver.com>
2026-01-20 10:21:12 +0100

    sql

--- cmake/SQL.cmake
+++ cmake/SQL.cmake
@@ -1,3 +1,24 @@
+include(FetchContent)
+  set(SQL_REPO "https://github.com/mysql/mysql-server")
+  set(SQL_TAG "mysql-8.4.7")
+  set(FETCHCONTENT_QUIET OFF)
+  set(FETCHCONTENT_BASE_DIR ${EXT_DOWNLOAD_DIR})
+  set(WITHOUT_SERVER ON)
+  set(WITH_BOOST ${POL_EXT_LIB_DIR}/boost_1_89_0)
+  set(WITH_UNIT_TESTS OFF)
+  FetchContent_Declare(
+    mysqlclient
+    GIT_REPOSITORY ${SQL_REPO}
+    GIT_TAG ${SQL_TAG}
+    GIT_SHALLOW TRUE
+  )
+  FetchContent_MakeAvailable(mysqlclient)
+  add_library(libsql SHARED IMPORTED)
+  add_dependencies(libsql mysqlclient)
+    set_target_properties(libsql PROPERTIES
+      INTERFACE_LINK_LIBRARIES mysqlclient
+    )
+  return()
 
 if(NOT HAVE_MYSQL)
   return()
