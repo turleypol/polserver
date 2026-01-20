1e337439fb922582972e74c77582556dab2cbc23
turleypol <turley@polserver.com>
2026-01-20 18:56:42 +0100

    s1

--- cmake/SQL.cmake
+++ cmake/SQL.cmake
@@ -9,11 +9,11 @@
     GIT_TAG          ${SQL_TAG}
     # only valid option if its a tag or branch not a commit
     GIT_SHALLOW      TRUE
-    PREFIX           sql
+    PREFIX           "${SQL_SOURCE_DIR}/build"
     LIST_SEPARATOR |
     CMAKE_ARGS       ${SQL_ARGS}
     SOURCE_DIR       "${SQL_SOURCE_DIR}"
-    BUILD_IN_SOURCE  1
+    #    BUILD_IN_SOURCE  1
     INSTALL_COMMAND  ${CMAKE_COMMAND} --build . --config Release --target install
     BUILD_BYPRODUCTS ${SQL_LIB}
     EXCLUDE_FROM_ALL 1
