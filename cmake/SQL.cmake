4f0e8b8fb29361a907360d0072b1d54e1460ba61
turleypol <turley@polserver.com>
2026-01-20 17:10:48 +0100

    s2

--- cmake/SQL.cmake
+++ cmake/SQL.cmake
@@ -1,23 +1,28 @@
-include(FetchContent)
   set(SQL_REPO "https://github.com/mysql/mysql-server")
   set(SQL_TAG "mysql-8.4.7")
-  set(FETCHCONTENT_QUIET OFF)
-  set(FETCHCONTENT_BASE_DIR ${EXT_DOWNLOAD_DIR})
-  set(WITHOUT_SERVER ON)
-  set(WITH_BOOST ${POL_EXT_LIB_DIR}/boost_1_89_0)
-  set(WITH_UNIT_TESTS OFF)
-  FetchContent_Declare(
-    mysqlclient
-    GIT_REPOSITORY ${SQL_REPO}
-    GIT_TAG ${SQL_TAG}
-    GIT_SHALLOW TRUE
+  set (SQL_SOURCE_DIR "${POL_EXT_LIB_DIR}/${SQL_TAG}")
+  set(SQL_ARGS "-DWITHOUT_SERVER=ON -DWITH_BOOST=${POL_EXT_LIB_DIR}/boost_1_89_0 -DWITH_UNIT_TESTS=OFF")
+  set(SQL_LIB "${SQL_SOURCE_DIR}/sql.so")
+  if(NOT EXISTS ${SQL_LIB})
+  ExternalProject_Add(mysqlclient_ext
+    GIT_REPOSITORY   ${SQL_REPO}
+    GIT_TAG          ${SQL_TAG}
+    # only valid option if its a tag or branch not a commit
+    GIT_SHALLOW      TRUE
+    PREFIX           sql
+    LIST_SEPARATOR |
+    CMAKE_ARGS       ${SQL_ARGS}
+    SOURCE_DIR       "${SQL_SOURCE_DIR}"
+    BUILD_IN_SOURCE  1
+    INSTALL_COMMAND  ${CMAKE_COMMAND} --build . --config Release --target install
+    BUILD_BYPRODUCTS ${SQL_LIB}
+    EXCLUDE_FROM_ALL 1
   )
-  FetchContent_MakeAvailable(mysqlclient)
+else()
+  message("  - already built")
+endif()
   add_library(libsql SHARED IMPORTED)
-  add_dependencies(libsql mysqlclient)
-    set_target_properties(libsql PROPERTIES
-      INTERFACE_LINK_LIBRARIES mysqlclient
-    )
+  add_dependencies(libsql mysqlclient_ext)
   return()
 
 if(NOT HAVE_MYSQL)
