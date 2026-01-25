996d96a4cc63c829e4ae2ca48b4e29cd517e5e39
turleypol <turley@polserver.com>
2026-01-25 11:06:48 +0100

    f

--- pol-core/pol/xmlfilescrobj.cpp
+++ pol-core/pol/xmlfilescrobj.cpp
@@ -102,10 +102,9 @@ Bscript::BObjectImp* BXMLfile::call_method_id( const int id, Executor& ex, bool 
         BStruct* attr = static_cast<BStruct*>( ex.getParamImp( 1, Bscript::BObjectImp::OTStruct ) );
         if ( attr )
         {
-          for ( const auto& citr : attr->contents() )
+          for ( const auto& [name, refobj] : attr->contents() )
           {
-            const std::string& name = citr.first;
-            Bscript::BObjectImp* ref = citr.second->impptr();
+            Bscript::BObjectImp* ref = refobj->impptr();
             if ( ref->isa( Bscript::BObjectImp::OTLong ) )
               elem->SetAttribute( name, static_cast<BLong*>( ref )->value() );
             else if ( ref->isa( Bscript::BObjectImp::OTDouble ) )
