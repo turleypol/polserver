ee12b1340a44499d9239174ee762903d69885f17
turleypol <turley@polserver.com>
2026-03-24 21:22:03 +0100

    zlibup

--- cmake/ZLib.cmake
+++ cmake/ZLib.cmake
@@ -3,7 +3,7 @@
 if (${windows})
   message("* libz")
   set(ZLIB_REPO "https://github.com/madler/zlib")
-  set(ZLIB_TAG "v1.3.1")
+  set(ZLIB_TAG "v1.3.2")
   set(ZLIB_SOURCE_DIR "${EXT_DOWNLOAD_DIR}/zlib-${ZLIB_TAG}")
 
   set(ZLIB_INSTALL_DIR "${ZLIB_SOURCE_DIR}/builds")
